ed6ed4e61a069850ce3c209ae1200ce1b82a886d
mikedcurry <mdcurry1138@gmail.com>
2024-10-15 13:26:42 -0400

    safety 1st, env test3

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from secrets import *
-from getpass import getpass
+from dotenv import load_dotenv
 import os
 
-OPENAI_API_KEY = Openai_key
-OPENAI_API_KEY = getpass()
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+# OPENAI_API_KEY = Openai_key
+# OPENAI_API_KEY = getpass()
+# os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv('API_SECRET')
+
+print('API_KEY: ', api_key)
+print("API_SECRET: ", api_secret)
